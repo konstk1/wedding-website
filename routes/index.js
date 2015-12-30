19e1deb9fbba2423c06d940f0d693ca2f72f3766
Konstantin Klitenik <konstantin@bytelight.com>
2015-12-30 16:09:24 -0500

    Weather, buttons, layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var Forecast = require('forecast');
+
+var forecast = new Forecast({
+  service: 'forecast.io',
+  key: 'b2f3210058fc18d72d41d32d9c06d6f8',
+  units: 'fahrenheit',
+  cache: true,
+  ttl: {
+    minutes: 60
+  }
+});
+
+var weddingDate = Date.parse("22-May-2016 17:00:00") / 1000;
+var newportLatLon = [41.529462, -71.272534];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Steph + Kon' });
+  forecast.get(newportLatLon, function(err, weather) {
+    if (err) return console.dir(err);
+    currentTemp = Math.round(weather.currently.temperature);
+    forecast.get(newportLatLon.concat(weddingDate), function(err, weather) {
+      if (err) return console.dir(err);
+      futureTemp = Math.round(weather.currently.temperature);
+      res.render('index', { title: 'Steph + Kon', temps: {current: currentTemp, future: futureTemp} });
+    });
+  });
 });
 
 module.exports = router;
