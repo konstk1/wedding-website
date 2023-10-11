372758b9f2d0a5c8426d87bd3db92bac9b3f8525
Konstantin Klitenik <5853429+konstk1@users.noreply.github.com>
2023-10-11 09:10:28 -0400

    Remove mongo and forecast integrations

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var monk = require('monk');
+// var monk = require('monk');
 
 var router = express.Router();
-var db = monk(process.env.MONGOLAB_URI);
-var collection = db.get('rsvp');
+// var db = monk(process.env.MONGOLAB_URI);
+// var collection = db.get('rsvp');
 
-router.get('/', function(req, res, next) {
-    console.log('Admin here');
+// router.get('/', function(req, res, next) {
+//     console.log('Admin here');
 
-    var rsvps = collection.find({},{}, function(e, docs) {
-       res.render('admin', {title: 'Admin', rsvps: docs});
-    });
-});
+//     var rsvps = collection.find({},{}, function(e, docs) {
+//        res.render('admin', {title: 'Admin', rsvps: docs});
+//     });
+// });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var monk = require('monk');
-var db = monk(process.env.MONGOLAB_URI);
-var rsvps = db.get('rsvp');
+// var monk = require('monk');
+// var db = monk(process.env.MONGOLAB_URI);
+// var rsvps = db.get('rsvp');
 var router = express.Router();
 var Forecast = require('forecast');
 
@@ -20,51 +20,51 @@ var newportLatLon = [41.529462, -71.272534];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  forecast.get(newportLatLon, function(err, weather) {
-    if (err) return console.dir(err);
-    currentTemp = Math.round(weather.currently.temperature);
-    forecast.get(newportLatLon.concat(weddingDate), function(err, weather) {
-      if (err) return console.dir(err);
-      futureTemp = Math.round(weather.currently.temperature);
-      var hourlyWeather = [];
-      weather.hourly.data.forEach(function(d) {
-        if (d.time >= 1463943600) {
-          var time = new Date(d.time * 1000);
-          if (time.getHours() % 2 != 0) {
-            var c = {
-              hour: time.getHours() - 12,
-              temp: Math.round(d.temperature),
-              cond: d.icon,
-              precip: Math.round(d.precipProbability * 100),
-              wind: Math.round(d.windSpeed)
-            };
-            hourlyWeather.push(c);
-          }
-        }
-      });
+  // forecast.get(newportLatLon, function(err, weather) {
+  //   if (err) return console.dir(err);
+  //   currentTemp = Math.round(weather.currently.temperature);
+  //   forecast.get(newportLatLon.concat(weddingDate), function(err, weather) {
+  //     if (err) return console.dir(err);
+  //     futureTemp = Math.round(weather.currently.temperature);
+  //     var hourlyWeather = [];
+  //     weather.hourly.data.forEach(function(d) {
+  //       if (d.time >= 1463943600) {
+  //         var time = new Date(d.time * 1000);
+  //         if (time.getHours() % 2 != 0) {
+  //           var c = {
+  //             hour: time.getHours() - 12,
+  //             temp: Math.round(d.temperature),
+  //             cond: d.icon,
+  //             precip: Math.round(d.precipProbability * 100),
+  //             wind: Math.round(d.windSpeed)
+  //           };
+  //           hourlyWeather.push(c);
+  //         }
+  //       }
+  //     });
 
-      res.render('index', { title: 'Steph + Kon', temps: {current: currentTemp, future: futureTemp}, hourly_weather: hourlyWeather });
-    });
-  });
+  //   });
+  // });
+  res.render('index', { title: 'Steph + Kon', temps: {current: 70, future: 70}, hourly_weather: [] });
 });
 
-router.get('/rsvp', function(req, res, next) {
-  res.render('rsvp_mobile', {title: 'RSVP', env: "test"});
-});
+// router.get('/rsvp', function(req, res, next) {
+//   res.render('rsvp_mobile', {title: 'RSVP', env: "test"});
+// });
 
-router.post('/rsvp', function(req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
+// router.post('/rsvp', function(req, res, next) {
+//   res.setHeader('Content-Type', 'application/json');
 
-  rsvps.insert(req.body, function(err, doc) {
-    if (err) {
-      console.log('Error: ' + err);
-      res.send('{"status": "insert failed"}');
-    } else {
-      res.send('{"status": "success"}');
-      console.log('Success: ' + JSON.stringify(doc));
-    }
-  });
+//   rsvps.insert(req.body, function(err, doc) {
+//     if (err) {
+//       console.log('Error: ' + err);
+//       res.send('{"status": "insert failed"}');
+//     } else {
+//       res.send('{"status": "success"}');
+//       console.log('Success: ' + JSON.stringify(doc));
+//     }
+//   });
 
-});
+// });
 
 module.exports = router;
